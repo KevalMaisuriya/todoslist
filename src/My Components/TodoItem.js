7f03c08d684056a3e11658371a6cddfceeee46b2
Keval Maisuriya <kevalmaisuriyaelsner@gmail.com>
2022-06-28 17:21:50 +0530

    Upload

--- src/My Components/TodoItem.js
+++ src/My Components/TodoItem.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function TodoItem() {
+export const TodoItem = ({todo,onDelete}) => {
   return (
-    <div>Todo Works</div>
+    <div>
+      <h4>{todo.title}</h4>
+      <p>{todo.desc}</p>
+      <button className="btn btn-sm btn-danger" onClick={() => {onDelete(todo)} }>Delete</button>
+    </div>
   )
 }
